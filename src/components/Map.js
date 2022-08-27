b5e0734575ec6a33dd6d4df976761ce98f87f11c
Alfredo Molina <linklloydgamer@gmail.com>
2022-08-27 10:37:54 -0500

    Corrige error con google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import GoogleMaps from "simple-react-google-maps"
+// import GoogleMaps from "simple-react-google-maps"
 export default class Maps extends Component {
     render() {
         return (
             <div className="container">
-                <GoogleMaps
+                {/* <GoogleMaps
                     apiKey={"AIzaSyDAqinXwCCK044Ajlj0dZ1OxghrkZboCls"}
                     style={{ height: "500px" , width: "800px"}}
                     zoom={18}
@@ -13,7 +13,7 @@ export default class Maps extends Component {
                         lng: -99.4849223,
                     }}
                     markers={{ lat: 17.5347404, lng: -99.4849223 }}
-                    />
+                    /> */}
             </div>
         );
     }
