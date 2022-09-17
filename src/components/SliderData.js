c01f606a40dcbd13c09f29fb3750d1071f2aa662
Alfredo Molina <linklloydgamer@gmail.com>
2022-09-16 20:23:31 -0500

    Add more images for slider

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -10,5 +10,14 @@ export const SliderData = [
     },
     {
         image: "https://image.shutterstock.com/image-photo/decorative-fifteen-year-birthday-cake-600w-575426785.jpg"
+    },
+    {
+        image: "https://thumbs.dreamstime.com/z/quinceanera-que-sube-los-pasos-62422805.jpg"
+    },
+    {
+        image: "https://i.pinimg.com/originals/78/c7/cf/78c7cf5de33786b62c718d57947163e0.jpg"
+    },
+    {
+        image: "https://lanoticia.com/wp-content/uploads/2022/05/latina-xv-atropellada.jpg"
     }
 ]
